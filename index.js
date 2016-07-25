118e8f1417ea33c757adfefe8e4c446ea57a1004
greatermeans <gakouri@gmail.com>
2016-07-25 15:11:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+	var counter = 0
+
+	function addBump(argument) {
+		counter += 1
+	}
+	function getBumps(argument) {
+		return counter
+	}
+
+	return {
+		addBump,
+		getBumps
+	}
+}
+
+function createAnimal(animalType) {
+	return ((deadlyDevice) => {
+		var obj = {}
+		{
+			obj.animalType = animalType
+			obj.deadlyDevice = deadlyDevice
+		}
+		return obj
+	})
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
